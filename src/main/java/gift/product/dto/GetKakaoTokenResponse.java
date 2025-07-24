43e9c175b7cc53b2cc523863bcccf83af91d62b9
Lee Si Woong <silverttthin@naver.com>
2025-07-24 21:37:36 +0900

    feat: 토큰 api 응답 dto 구현 - 결과값이 스네이크 케이스로 와서 역직렬화 할 때 자바의 camelCase 위해 JsonProperty 어노테이션 사용

---
+++ src/main/java/gift/product/dto/GetKakaoTokenResponse.java
@@ -0,0 +1,27 @@
+package gift.product.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GetKakaoTokenResponse(
+	@JsonProperty("token_type")
+	String tokenType,
+
+	@JsonProperty("access_token")
+	String accessToken,
+
+	@JsonProperty("id_token")
+	String idToken,
+
+	@JsonProperty("expires_in")
+	Integer expiresIn,
+
+	@JsonProperty("refresh_token")
+	String refreshToken,
+
+	@JsonProperty("refresh_token_expires_in")
+	Integer refreshTokenExpiresIn,
+
+	@JsonProperty("scope")
+	String scope
+) {
+}
