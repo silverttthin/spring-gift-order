c0340a0a53df60cc14684fc36fc898f7059972cd
Lee Si Woong <silverttthin@naver.com>
2025-07-28 17:04:04 +0900

    feat: 카카오 유저정보 가져오는 서비스 함수 추가

--- src/main/java/gift/product/service/KakaoService.java
+++ src/main/java/gift/product/service/KakaoService.java
@@ -2,8 +2,10 @@ package gift.product.service;
 
 
 import gift.product.dto.GetKakaoTokenResponse;
+import gift.product.dto.GetKakaoUserInfoResponse;
 
 
 public interface KakaoService {
 	GetKakaoTokenResponse getKakaoToken(String code);
+	GetKakaoUserInfoResponse getKakaoUserInfo(String accessToken);
 }
--- src/main/java/gift/product/service/impl/WebClientKakaoImpl.java
+++ src/main/java/gift/product/service/impl/WebClientKakaoImpl.java
@@ -2,8 +2,11 @@ package gift.product.service.impl;
 
 import gift.product.dto.GetKakaoTokenApiResponse;
 import gift.product.dto.GetKakaoTokenResponse;
+import gift.product.dto.GetKakaoUserInfoResponse;
 import gift.product.service.KakaoService;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,7 +19,7 @@ public class WebClientKakaoImpl implements KakaoService {
 	@Value("${kakao.redirect-uri}")
 	private String redirectUri;
 
-
+	@Override
 	public GetKakaoTokenResponse getKakaoToken(String code) {
 		try {
 			GetKakaoTokenApiResponse response =  WebClient.create("https://kauth.kakao.com")
@@ -28,16 +31,34 @@ public class WebClientKakaoImpl implements KakaoService {
 					.queryParam("code", code)
 					.queryParam("redirect_uri", redirectUri)
 					.build(true))
-				.header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+				.header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
 				.retrieve()
 				.bodyToMono(GetKakaoTokenApiResponse.class)
 				.block();
-
 			return new GetKakaoTokenResponse(response.accessToken());
 		} catch (Exception e) {
 			throw new IllegalArgumentException("잘못된 코드입니다. 코드를 재발급받아 다시 시도해주세요", e.getCause());
 		}
 	}
 
+
+	@Override
+	public GetKakaoUserInfoResponse getKakaoUserInfo(String accessToken) {
+		try {
+			GetKakaoUserInfoResponse userInfo = WebClient.create("https://kapi.kakao.com")
+				.get()
+				.uri(uriBuilder -> uriBuilder
+					.path("/v2/user/me")
+					.build(true))
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken) // 액세스 토큰 인가
+				.header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
+				.retrieve()
+				.bodyToMono(GetKakaoUserInfoResponse.class)
+				.block();
+			return userInfo;
+		} catch (Exception e) {
+			throw new IllegalArgumentException("유저 정보를 불러오는데 실패했습니다. 토큰을 재발급받아 다시 시도해주세요.", e.getCause());
+		}
+	}
 
 }
